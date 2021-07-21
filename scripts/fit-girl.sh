23c76b1e2a6622bc7da254172e3f788b7765fef2
reggie25x <81920698+reggiiie@users.noreply.github.com>
2021-07-21 20:09:20 +0000

    uses pup included binary

--- scripts/fit-girl.sh
+++ scripts/fit-girl.sh
@@ -4,7 +4,7 @@
 curl -s "https://1337x.to/sort-category-search/FitGirl%20$1/Games/seeders/desc/1/" | 
 
 #extract links from html
-pup 'table a json{}' |
+"$(dirname "$0")"/pup 'table a json{}' |
 jq -r '.[].href' |
 grep -v "\/user\/FitGirl\/" | 
 tac | sed '/\/sub.*/d;/\/user\//,+1 d' | tac | 
