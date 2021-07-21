13083b3f272a3532404bfdcb16143581595c12cc
reggie25x <81920698+reggiiie@users.noreply.github.com>
2021-07-21 20:10:06 +0000

    uses pup included binary

--- scripts/rin.sh
+++ scripts/rin.sh
@@ -4,7 +4,7 @@
 python "$(dirname "$0")"/rin.py "$1" | 
 
 #extract links
-pup 'table.tablebg a json{}' | 
+"$(dirname "$0")"/pup 'table.tablebg a json{}' | 
 jq -r '(.[] | select(.class=="topictitle")) .href' |
 sed -e 's/^.//;s/^/https\:\/\/cs\.rin\.ru\/forum/;s/\&amp\;/\&/g' |
 #get rid of newline at the end
