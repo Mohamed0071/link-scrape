9bce134b2582fe9f827ef2a78a88c9453513a384
reggiiie <MvpoWYM3OJ@keemail.me>
2021-07-23 02:22:25 +0200

    gog search result improvements

--- scripts/gog-games.sh
+++ scripts/gog-games.sh
@@ -7,5 +7,10 @@ curl -s "https://gog-games.com/search/$(printf '%s\n' "$1" | sed -e 's/ /\%20/g'
 "$(dirname "$0")"/pup 'div.game-blocks.grid-view a json{}' |
 jq -r '.[].href' | 
 sed -e 's/^/https\:\/\/gog\-games\.com/' |
+
+#gog-games search results are bad, if they include more than one word 
+#sed removes trailing and leading whitespaces and then replaces all remaining spaces with underscores and feeds the output to grep 
+grep -Ei "$(printf '%s\n' "$1" | sed 's/^[ \t]*//;s/[ \t]*$//;s/ /\_/;')" |
+
 #get rid of newline at the end
-perl -pe 'chomp if eof'
+perl -pe 'chomp if eof' 
