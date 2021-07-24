83215cfc4e90e9c28601648674e5b52bdc5c36cc
reggiiie <MvpoWYM3OJ@keemail.me>
2021-07-24 20:53:27 +0200

    improved error handling

--- search.sh
+++ search.sh
@@ -5,16 +5,20 @@ exclude=("rin.sh");
 
 dir="$(pwd)";
 #cd into scripts directory
-cd "$(dirname "$0")"/scripts || exit;
+cd "$(dirname "$0")"/scripts || exit 1;
 
 #check if there are any arguments left
 argCheck() {
     if [ "$#" -eq 0 ]
     then
-        printf "no argument supplied\n";
+        printf "%s %s: not enough arguments supplied\n" "$0" "$stage";
+        cd "$dir" || exit 1;
         exit 1;
     fi
 }
+
+#checks if there are any arguments
+argCheck "$@";
 
 #loops until there are no more arguments
 while [ $# -gt 0 ]
@@ -26,7 +30,8 @@ do
             exit 0;
         ;;
         -e|--exclude)
-            shift
+            stage="$1";
+            shift;
             
             #check if there is any argument after flag
             argCheck "$@";
@@ -47,11 +52,11 @@ do
                     exclude+=("$file");
                 else
                     #if doesn't exist print error and exit
-                    printf '\"%s\" not found in the script directory\n' "$1";
+                    printf '%s %s: \"%s\" not found in the script directory\n' "$0" "$stage" "$file";
                     exit 1;
                 fi
             done
-            shift
+            shift;
         ;;
         *)
             break;
@@ -59,6 +64,7 @@ do
     esac
 done
 
+argCheck "$@";
 arg="$1";
 
 #loops through all .sh files in directory
@@ -75,4 +81,4 @@ for i in *.sh; do
     jq --arg script "$i" -Rs '{($script):split("\n")}' < <(sh "$i" "$arg");
 done
 
-cd "$dir" || exit
+cd "$dir" || exit 1
